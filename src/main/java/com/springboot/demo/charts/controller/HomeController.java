d0e84015b671996aa0717fd6844000aaef21b5aa
gardiary <gardiary@gmail.com>
2019-01-22 15:40:00 +0700

    Update maven and boot version

--- src/main/java/com/springboot/demo/charts/controller/HomeController.java
+++ src/main/java/com/springboot/demo/charts/controller/HomeController.java
@@ -3,8 +3,6 @@ package com.springboot.demo.charts.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Map;
-
 /**
  * Created by gardiary on 22/01/19.
  */
